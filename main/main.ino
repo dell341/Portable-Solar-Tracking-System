7941335e123b74e3681bc122e00a5835b972ad74
Nathan Louis <nlouis24@gmail.com>
2017-02-25 17:07:05 -0500

    Added main Arduino code file

---
+++ main/main.ino
@@ -0,0 +1,21 @@
+/*
+ * Portable Solar Tracking System - Main
+ * 
+ * This program will contain the main control for the Photovoltaic (PV)
+ * tracking system. The Arduino will communicate with the stepper motor drives,
+ * a 16-bit ADC, hall effect sensors, and any other peripheral devices deemed
+ * necessary. 
+ */
+
+ #include <Stepper.h> //Stepper motor driver class provided by Arduino
+
+ 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
